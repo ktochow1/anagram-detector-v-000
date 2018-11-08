7354c2235665a22c3d6a4786c718455c430269c4
ktochow1 <ktochow1@binghamton.edu>
2018-11-08 19:52:50 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -22,9 +22,12 @@ class Anagram
 # match.(["hello", "goodbye", "yellow", "dog"]) || variable = array 
 #.keep_if looks at array and fishes out what you're looking for
 #ex: [a,b,c,d,e,f,g].keep_if {|char| char =~ /aeiou/} => ["a", "e"]
+  #-- don't worry about that notation, that's whats going on inside method -- 
+#keep_if deletes every element of self for which the given block evaluates to false!!!!!
 
-#=~ matches rxp with str ???? 
-
+#arr.keep_if do |str|
+  #name.split('').sort -- splits every item into array in alphabetical order (name is the word that you're passing into the method)
+  #and compares it to the string's name value that's inside the array you're comparing values with 
 
 
 
