86bdac5611294138d109d418e5fe4cf19fca0f29
ktochow1 <ktochow1@binghamton.edu>
2018-11-08 19:29:15 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,17 +1,22 @@
 # Your code goes here!
+
+require 'pry'
 class Anagram
   attr_reader :name
  
   def initialize(name)
     @name = name
   end
-#initialize will 
+#initialize will trigger .new and make up a new instance of the class
   
   def match(arr)
-    arr.keep_if do |str| 
-      name.split('').sort == str.split('').sort
+    arr.keep_if do |str|
+      binding.pry
+      # name.split('').sort == str.split('').sort
     end
   end
+#.match takes in an array of strings
+
 
   
 end
